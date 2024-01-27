fe6a78728f75760818358a342fbe400d9b5db13d
satya <satyanandgupta845@gmail.com>
2024-01-28 05:03:53 +0530

    deleted file

--- child_p.js
+++
@@ -1,9 +0,0 @@
-const cp = require('child_process')
-
-// cp.execSync('calc')
-// cp.execSync('start chrome')
-// cp.execSync('start chrome https://youtube.com')
-// cp.execSync('start chrome https://github.com/Satya7390')
- 
-
-console.log('Output: ' + cp.execSync('node demo.js'))
--- os.js
+++
@@ -1,9 +0,0 @@
-const os = require('os')
-
-console.log(os.arch())//architecture (64 or 32)
-
-console.log(os.platform())
-console.log(os.networkInterfaces())
-console.log(os.cpus())//for system details 
-console.log(os.totalmem())//total memory space
-console.log(os.freemem())//free memory space
