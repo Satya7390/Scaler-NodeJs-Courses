fdc4b02b5f4966d3d7e2332fcba03eb2e09a3c68
satya <satyanandgupta845@gmail.com>
2024-01-30 14:03:18 +0530

    added calculator file

---
+++ result.js
@@ -0,0 +1,8 @@
+const calculator = require('./calculator')
+calculator.addition(4,5)
+calculator.subtraction(4,5)
+calculator.multiplication(4,5)
+calculator.division(4,5)
+calculator.modulus(10,5)
+calculator.division(5,77)
+calculator.modulus(5,108)
